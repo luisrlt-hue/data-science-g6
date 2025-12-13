6e9f92f83e9fd62b50fc1e6e4a54d3714219b584
luisrlt-hue <luisrlt@gmail.com>
2025-12-12 22:22:25 -0500

    DIA4

---
+++ dia4/1-listas.py
@@ -0,0 +1,25 @@
+dias=["Lunes","Martes","Miercoles","Jueves","Viernes"]
+
+#Mostrando Valores
+
+print(dias[0])
+
+#agregar elemntos a la lista
+
+dias.append("Sabado")
+dias.append("Domingo")
+
+#eliminar elementos de la lista
+
+dias.pop(2)
+del dias[0:2]
+
+#mostrar todos los valores de al lista
+
+for dia in dias:
+    print(dia)
+    
+    
+
+
+    
