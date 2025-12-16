0a20a372208cca4de155708bb4c498ce4c80d972
luisrlt-hue <luisrlt@gmail.com>
2025-12-16 12:28:49 -0500

    tkinter

---
+++ dia1/6-tkinter.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+#creamos un objeto de la clase Tk
+app = Tk()
+
+#titulo de la ventana
+app.title("Mi primera aplicacion con Tkinter")
+
+#dimensiones de la ventana
+app.geometry("400x300")
+
+#mostrar la ventana
+app.mainloop()
