c09c973656391f3980188854444f6cc17e567533
luisrlt-hue <luisrlt@gmail.com>
2026-01-03 22:50:24 -0500

    variables de entorno

--- dia4/api/2_requests-ruc.py
+++ dia4/api/2_requests-ruc.py
@@ -1,8 +1,11 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 API_URL = 'https://apiperu.dev/api/ruc'
-TOKEN = '22af9c82fc51e180cf597cc2b49e620197950f57a161216e3a2ff72ac91fa612'
-
+TOKEN = os.getenv("TOKEN")
 data_request = {
   "ruc":"20117592899"
 }
@@ -12,10 +15,10 @@ headers = {
     "Content-Type": "application/json"
 }
 
-response = requests.post(API_URL, json=data_request, headers=headers)
+response = requests.post(API_URL,json=data_request,headers=headers)
 
 if response.status_code == 200:
-    data=response.json()['data']
+    data = response.json()['data']
     print("="*50)
     print(f'RUC : {data['ruc']}')
     print(f'Razon Social : {data["nombre_o_razon_social"]}')
@@ -24,5 +27,3 @@ if response.status_code == 200:
     print(f'Provincia : {data["provincia"]}')
     print(f'Departamento : {data["departamento"]}')
     print(f'ubigeo : {data["ubigeo_sunat"]}')
-    
-    
