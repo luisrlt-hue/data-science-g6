3ebcde66c1ac033abbefd6a087b0a2dea89a53d2
luisrlt-hue <luisrlt@gmail.com>
2026-01-12 21:45:22 -0500

    dia1

---
+++ dia1/python-local/estadistica-python.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+datos=np.array([2,2,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7])
+
+# Media
+media=np.mean(datos)
+print(f"Media: {media}")
+
+# Mediana
+mediana=np.median(datos)
+print(f"Mediana: {mediana}")
+
+#moda
+valores, conteos = np.unique(datos, return_counts=True)
+moda = valores[np.argmax(conteos)]
+print(f"Moda: {moda}")
+
+
